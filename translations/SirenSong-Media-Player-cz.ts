f79ec6edcf0ac3a4ecf632f1d2679a44eb1073d9
LRockliffe <r0kk3rz@gmail.com>
2014-11-06 22:55:01 +0000

    Czech Translation
    Czech translation by Pavel Fric

---
+++ translations/SirenSong-Media-Player-cz.ts
@@ -0,0 +1,60 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="cs_CZ">
+<context>
+    <name>ArtistSelect</name>
+    <message>
+        <source>Add to Play Queue</source>
+        <translation>Přidat do seznamu skladeb</translation>
+    </message>
+    <message>
+        <source>Unknown Artist</source>
+        <translation>Neznámý umělec</translation>
+    </message>
+</context>
+<context>
+    <name>LibraryPage</name>
+    <message>
+        <source>Sort by Artist</source>
+        <translation>Třídit dle umělce</translation>
+    </message>
+    <message>
+        <source>Sort by Song</source>
+        <translation>Třídit dle písně</translation>
+    </message>
+    <message>
+        <source>Library</source>
+        <translation>Knihovna</translation>
+    </message>
+</context>
+<context>
+    <name>PlayerPage</name>
+    <message>
+        <source>Play Queue</source>
+        <translation>Přehrát seznam skladeb</translation>
+    </message>
+    <message>
+        <source>Clear Play Queue</source>
+        <translation>Vyprázdnit seznam skladeb</translation>
+    </message>
+    <message>
+        <source>Repeat: On</source>
+        <translation>Opakovat: Zapnuto</translation>
+    </message>
+    <message>
+        <source>Repeat: Off</source>
+        <translation>Opakovat: Vypnuto</translation>
+    </message>
+</context>
+<context>
+    <name>SongSelect</name>
+    <message>
+        <source>Add to Play Queue</source>
+        <translation>Přidat do seznamu skladeb</translation>
+    </message>
+    <message>
+        <source>Unknown Artist</source>
+        <translation>Neznámý umělec</translation>
+    </message>
+</context>
+</TS>
