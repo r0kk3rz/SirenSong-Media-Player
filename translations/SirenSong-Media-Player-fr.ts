ad334a733681375f10bd25bde53eb99192cffa5f
LRockliffe <r0kk3rz@gmail.com>
2014-12-17 18:59:56 +0000

    French Translation

---
+++ translations/SirenSong-Media-Player-fr.ts
@@ -0,0 +1,95 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr">
+<context>
+    <name>ArtistSelect</name>
+    <message>
+        <source>Add to Play Queue</source>
+        <translation>Ajouter à la liste de lecture</translation>
+    </message>
+    <message>
+        <source>Unknown Artist</source>
+        <translation>Artiste inconnu</translation>
+    </message>
+</context>
+<context>
+    <name>LibraryPage</name>
+    <message>
+        <source>Sort by Artist</source>
+        <translation>Trier par Artiste</translation>
+    </message>
+    <message>
+        <source>Sort by Song</source>
+        <translation>Trier par Titre</translation>
+    </message>
+    <message>
+        <source>Library</source>
+        <translation>Bibliothèque</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>PlayerPage</name>
+    <message>
+        <source>Play Queue</source>
+        <translation>Liste de lecture</translation>
+    </message>
+    <message>
+        <source>Clear Play Queue</source>
+        <translation>Vider la liste de lecture</translation>
+    </message>
+    <message>
+        <source>Repeat: On</source>
+        <translation>Répéter: On</translation>
+    </message>
+    <message>
+        <source>Repeat: Off</source>
+        <translation>Répéter: Off</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <source>Save</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Play Mode</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Auto Queue</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Repeat</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Default Library Menu</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Sort by Song</source>
+        <translation type="unfinished">Trier par Titre</translation>
+    </message>
+    <message>
+        <source>Sort by Artist</source>
+        <translation type="unfinished">Trier par Artiste</translation>
+    </message>
+</context>
+<context>
+    <name>SongSelect</name>
+    <message>
+        <source>Add to Play Queue</source>
+        <translation>Ajouter à la liste de lecture</translation>
+    </message>
+    <message>
+        <source>Unknown Artist</source>
+        <translation>Artiste inconnu</translation>
+    </message>
+</context>
+</TS>
