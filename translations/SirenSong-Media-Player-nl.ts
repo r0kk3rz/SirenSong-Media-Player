0d3d7ea37ad405456a4c482f7e10a882e73b9c51
LRockliffe <r0kk3rz@gmail.com>
2014-12-19 17:57:11 +0000

    Dutch Translation
    Dutch translation by Vistaus

---
+++ translations/SirenSong-Media-Player-nl.ts
@@ -0,0 +1,95 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="nl_NL">
+<context>
+    <name>ArtistSelect</name>
+    <message>
+        <source>Add to Play Queue</source>
+        <translation>Toevoegen aan afspeelwachtrij</translation>
+    </message>
+    <message>
+        <source>Unknown Artist</source>
+        <translation>Onbekende artiest</translation>
+    </message>
+</context>
+<context>
+    <name>LibraryPage</name>
+    <message>
+        <source>Sort by Artist</source>
+        <translation>Sorteren op artiest</translation>
+    </message>
+    <message>
+        <source>Sort by Song</source>
+        <translation>Sorteren op nummer</translation>
+    </message>
+    <message>
+        <source>Library</source>
+        <translation>Bibliotheek</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Instellingen</translation>
+    </message>
+</context>
+<context>
+    <name>PlayerPage</name>
+    <message>
+        <source>Play Queue</source>
+        <translation>Afspeelwachtrij</translation>
+    </message>
+    <message>
+        <source>Clear Play Queue</source>
+        <translation>Afspeelwachtrij wissen</translation>
+    </message>
+    <message>
+        <source>Repeat: On</source>
+        <translation>Herhalen: Aan</translation>
+    </message>
+    <message>
+        <source>Repeat: Off</source>
+        <translation>Herhalen: Uit</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <source>Save</source>
+        <translation>Opslaan</translation>
+    </message>
+    <message>
+        <source>Play Mode</source>
+        <translation>Afspeelmodus</translation>
+    </message>
+    <message>
+        <source>Auto Queue</source>
+        <translation>Automatisch in wachtrij zetten</translation>
+    </message>
+    <message>
+        <source>Repeat</source>
+        <translation>Herhalen</translation>
+    </message>
+    <message>
+        <source>Default Library Menu</source>
+        <translation>Standaard bibliotheekmenu</translation>
+    </message>
+    <message>
+        <source>Sort by Song</source>
+        <translation>Sorteren op nummer</translation>
+    </message>
+    <message>
+        <source>Sort by Artist</source>
+        <translation>Sorteren op artiest</translation>
+    </message>
+</context>
+<context>
+    <name>SongSelect</name>
+    <message>
+        <source>Add to Play Queue</source>
+        <translation>Toevoegen aan afspeelwachtrij</translation>
+    </message>
+    <message>
+        <source>Unknown Artist</source>
+        <translation>Onbekende artiest</translation>
+    </message>
+</context>
+</TS>
